f177a21c95c106d40144d35b6f2457eb3c637fa2
Catislin <caitlin.estrellita@gmail.com>
2017-10-31 12:07:17 -0700

    added paperkit card styling

--- src/components/contest-card.js
+++ src/components/contest-card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from "./style.css";
+import paperKit from "../../public/pk2-pro-html-v2/assets/css/paper-kit.css";
 import { Link } from 'react-router';
 //import { ContestCard } from '../../shared/components/ContestCard';
 import { ButtonToolbar, Button } from 'react-bootstrap';
@@ -11,8 +12,10 @@ export default class ContestCard extends Component {
 
   render () {
     return (
-      <Link to={ `/contest/${this.props.id}` } className={styles.contestCard}>
-        <h3> {this.props.name} </h3>
+      <Link to={ `/contest/${this.props.id}` } className={paperKit.card} data-background="color" data-color="orange" data-radius="none">
+        <div className={paperKit.cardBlock}>
+          <h3 className={paperKit.cardCategory}> {this.props.name} </h3>
+        </div>
       </Link>
     );
   }
