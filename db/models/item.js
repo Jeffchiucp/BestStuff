84d47a6c0d513e9aa47530996f0834ac3fde0530
Rohan Mishra <rohanmishra2017@gmail.com>
2017-11-01 13:15:55 -0700

    Removed associations from class model

--- db/models/item.js
+++ db/models/item.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Item.belongsToMany(models.Contest, {through: 'ContestItems'});
       }
     }
   });
